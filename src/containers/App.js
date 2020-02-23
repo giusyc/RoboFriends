dab5b06fb1e7aa4ca5d771bcc6cbbc5be6c16888
Giusy Castiglione <gc@datespotz.com>
2020-02-23 16:24:29 -0500

    robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import '../containers/App.css';
+import './App.css';
 
 class App extends Component {
 	constructor() {
@@ -26,11 +26,15 @@ class App extends Component {
 	}
 
 	render() {
-			const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+			const { robots, searchfield } = this.state;
+			const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
 
-	return (
+	return !robots.length ?
+		<h1>Loading</h1> :
+		
+	(
 		<div className="tc">
 			<h1 className="f1">RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
