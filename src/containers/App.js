865a0aa55c10602cf9321d6714651d94bfca1fee
Giusy Castiglione <gc@datespotz.com>
2020-02-23 21:14:37 -0500

    edited app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import './App.css';
+import ErrorBoundary from '../components/ErrorBoundary';
+import '../containers/App.css';
 
 class App extends Component {
 	constructor() {
@@ -30,24 +31,24 @@ class App extends Component {
 			const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
+	 
 
 	return !robots.length ?
-		<h1>Loading</h1> :
-		
+		<h1 className="tc">Loading</h1> :
+
 	(
 		<div className="tc">
 			<h1 className="f1">RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
 			<Scroll>
-			<CardList robots={filteredRobots} />
+				<ErrorBoundary>
+					<CardList robots={filteredRobots} />
+				</ErrorBoundary>
 			</Scroll>
 		</div>
 
-		)
+		);
 	}
 }
 
 export default App;
-
-
-
